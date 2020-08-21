b46136395fabd291d0840aa3887d958b635bf65e
张磊 <b15010820@njupt.edu.cn>
2020-08-21 16:11:43 +0800

    Life_Game(元胞自动机)--V1.0.0 1.4 CODING, TESTING, AND FURTHER REFINEMENT P1. Enter the Life program of this chapter on your computer and make sure that it works correctly.

---
+++ life_game/life_game/Life_Game.cpp
@@ -0,0 +1,39 @@
+#include "utility.h"
+#include "life.h"
+
+using namespace std;
+
+void instructions()
+/*
+Pre:  None.
+Post: Instructions for using the Life program have been printed.
+*/
+{
+	cout << "Welcome to Conway's game of Life." << endl;
+	cout << "This game uses a grid of size " << maxrow << " by " << maxcol << " in which" << endl;
+	cout << "each cell can either be occupied by an organism or not." << endl;
+	cout << "The occupied cells change from generation to generation" << endl;
+	cout << "according to the number of neighboring cells which are alive." << endl;
+}
+
+int main() // Program to play Conway's game of Life.
+/*
+Pre:  The user supplies an initial configuration of living cells.
+Post: The program prints a sequence of pictures showing the changes in
+      the configuration of living cells according to the rules for
+      the game of Life.
+Uses: The class Life and its methods initialize(), print(), and
+      update(); the functions instructions(), user_says_yes().
+*/
+{
+	Life configuration;
+	instructions();
+	configuration.initialize();
+	configuration.print();
+	cout << "Continue viewing new generations? " << endl;
+	while (user_says_yes()) {
+		configuration.update();
+		configuration.print();
+		cout << "Continue viewing new generations? " << endl;
+	}
+}
---
+++ life_game/life_game/utility.h
@@ -0,0 +1,12 @@
+#include <iostream>		// standard iostream operations
+#include <limits>		// numeric limits
+#include <cmath>		// mathematical functions
+#include <cstdlib>		// C-string functions
+#include <cstddef>		// C library language support
+#include <fstream>		// file input and output
+#include <cctype>		// character classification
+#include <ctime>		// date and time functions
+
+bool user_says_yes();
+
+//enum Error_code { success, fail, range_error, underflow, overflow, fatal, not_present, duplicate_error, entry_inserted, entry_found, internal_error };
